522697200b2d26753775f0aa06b156bd3ffeacdf
Zhenbang Chen <zbchen.zju@gmail.com>
2017-06-12 01:15:19 +0800

    improvements

--- Next Permutation/next_permutation.cpp
+++ Next Permutation/next_permutation.cpp
@@ -10,7 +10,9 @@ public:
             while (nums[++j]>nums[i-1] && j < size);
             swap(nums[i-1],nums[j-1]);
         }
-        for(int k =i; k<= (size+i-2)/2; ++k)
-            swap(nums[k],nums[size-1-(k-i)]);
+        // 由于已经是降序排序，所以只要 reverse 就好，不需要 sort
+        reverse(nums.begin()+i,nums.end());
+        //for(int k =i; k<= (size+i-2)/2; ++k)
+        //    swap(nums[k],nums[size-1-(k-i)]);
     }
 };
