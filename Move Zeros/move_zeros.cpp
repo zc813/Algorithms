811fbb55185a22457ba572a0283ee2c4cf6bd256
Zhenbang Chen <zbchen.zju@gmail.com>
2017-06-12 01:21:52 +0800

    Create move_zeros.cpp

---
+++ Move Zeros/move_zeros.cpp
@@ -0,0 +1,13 @@
+// 有趣的地方在 line 9
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int index = 0;
+        for(int i = 0 ; i<nums.size(); ++i)
+            if (nums[i] != 0)
+                nums[index++] = nums[i];
+        for(int i = index; i< nums.size();++i)
+            nums[i]=0;
+    }
+};
